bdaaaa2c8b09e934190486aba06f7890db10a52f
ShaiFletcher <shaixperiaz1@gmail.com>
2021-01-19 22:14:42 +0200

    q10 commit

---
+++ q10.js
@@ -0,0 +1,20 @@
+function run() {
+    const input = document.getElementById("input").value;
+    const output = document.getElementById("output");
+    let out = "";
+/**/
+    // write your code here
+    //out = input + 5;
+    var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+ 
+    for (var i in a) 
+    {
+        console.log("row " + i);
+    for (var j in a[i]) 
+        {
+        console.log(" " + a[i][j]);
+        }
+    }
+    /**/
+        output.innerText = out;
+    }
