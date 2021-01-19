08ad2c1428c1cec2e23bf571881706751c9cd60b
ShaiFletcher <shaixperiaz1@gmail.com>
2021-01-19 18:37:29 +0200

    Update q1.js

--- q1.js
+++ q1.js
@@ -4,8 +4,15 @@ function run() {
     let out = "";
 /**/
     // write your code here
-    // out = input + 5;
+    //out = input + 5;
 
+    var is_array = input => {
+        if (toString.call(input) === "[object Array]")
+          return true;
+        return false;   
+          };
+      console.log(is_array('w3resource'));
+      console.log(is_array([1, 2, 4, 0]));
 /**/
     output.innerText = out;
 }
